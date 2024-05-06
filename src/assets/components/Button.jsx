5e76401a8ee1dae0ae3f210a0c6051f499fbdd44
Mahera Nayan <136584376+itznayan@users.noreply.github.com>
2024-05-06 20:00:29 +0530

    Done

--- src/assets/components/Button.jsx
+++ src/assets/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ children, ...props }) => {
   return (
     <button
-      className="px-4 text-gray-200 py-2 text-xs md:text-base rounded-md bg-gray-800 hover:bg-stone-700 duration-500 hover:text-stone-200"
+      className="px-4 text-gray-200 py-2 text-xs md:text-base rounded-md bg-gray-800 hover:bg-stone-800 duration-500 hover:text-stone-200"
       {...props}
     >
       {children}
